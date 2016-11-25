7f7bcafb170c2d676f26ea1dc8ab708a167efaf4
mosfiqur-rahman <where.is.mushfique@gmail.com>
2016-11-25 00:33:21 -0500

    new algo

---
+++ fibonacci_huge.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long get_fibonacci_huge(long long n, long long m)
+{
+	 vector<long long>fibonacci(n+1);
+	 vector<long long>remainder(m+1);
+
+	 if(n == 0)
+		 return 0;
+
+	 fibonacci[0] = 0;
+	 fibonacci[1] = 1;
+
+	 remainder[0] = 0;
+	 remainder[1] = 1;
+
+	 int counter = 2;
+
+	 while(counter <= n)
+	 {
+		 fibonacci[counter] = fibonacci[counter - 1] + fibonacci[counter - 2];
+		 remainder[counter] = fibonacci[counter] % m;
+
+		 //if( counter == n)
+			 //return remainder[counter];
+
+		 if(remainder[counter - 1] == 0 && remainder[counter] == 1)
+			 break;
+
+		 counter++;
+	 }
+
+	 int smaller_n = (int)(n % (counter - 1)); // The period length is [counter -1]
+
+	 return fibonacci[smaller_n] % m;
+
+}
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+    cout << get_fibonacci_huge(n, m) << '\n';
+}
+
